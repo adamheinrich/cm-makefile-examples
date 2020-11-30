77af3517a17051f7ee637b8b9294fec74f8243bf
Adam Heinrich <adam@adamh.cz>
2020-11-30 10:08:21 +0100

    Update libopencm3

--- 1bitsy/main.c
+++ 1bitsy/main.c
@@ -12,7 +12,7 @@ void sys_tick_handler(void)
 
 int main(void)
 {
-	rcc_clock_setup_hse_3v3(&rcc_hse_25mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+	rcc_clock_setup_pll(&rcc_hse_25mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 
 	/* Enable clock for GPIOA and set GPIO pin PA8 as output: */
 	rcc_periph_clock_enable(RCC_GPIOA);
